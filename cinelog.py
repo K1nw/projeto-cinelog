dfb9b6f194e36aec6d2b421db038f8d50ab572ce
kin <nathancosta207@gmail.com>
2026-08-08 02:12:22 -0300

    Opção de Excluir filmes incluída

--- cinelog.py
+++ cinelog.py
@@ -6,7 +6,8 @@ def mostrar_menu():
     print("1 - Adicionar Filmes")
     print("2 - Listar Filmes")
     print("3 - Editar Filmes")
-    print("4 - Sair")
+    print("4 - Excluir Filmes")
+    print("5 - Sair")
 
 #função de arquivo
 def salvar_filmes():
@@ -81,7 +82,25 @@ def editar_filme():
     with open("filmes.json", "w", encoding="utf-8") as arquivo:
         json.dump(filmes, arquivo, ensure_ascii=False, indent=4)
 
-print("Seu filme foi Editado com sucesso! ")
+def excluir_filme():
+    if not filmes:
+        print("Nenhum filme para excluir.")
+        return
+    print("Escolha o Número do filme que deseja excluir:")
+    for numero, filme in enumerate(filmes, start=1):
+        print(f"{numero} - {filme['titulo']}")
+
+    opcao = int(input("Digite o número do filme para Excluir: "))
+    filme = filmes[opcao - 1]
+    
+    print(f"Deseja mesmo excluir o filme: {filme['titulo']} (número {opcao})? s/n")
+    confirmar = input().strip().lower()
+    if confirmar not in ['s', 'y', 'sim', 'yes']:
+        print("Exclusão cancelada.")
+        return
+    filmes.pop(opcao - 1)
+    salvar_filmes()
+    print("Filme excluído com sucesso!")
 
 # Menu principal
 while True:
@@ -95,8 +114,10 @@ while True:
     elif opcao == "3":
         editar_filme()
     elif opcao == "4":
+        print("Excluir filmes")
+        excluir_filme()
+    elif opcao == "5":
         print("Saindo do Cinelog")
-    
         break
     else:
         print("Opção inválida.")
